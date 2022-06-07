fa5dd1218c921d8dc7c60e3de73083fc30f24e08
kevser canca <kevsercanca@gmail.com>
2022-06-07 17:52:44 +0300

    07.06.2022 GenericRepository eklendi.

---
+++ DataAccessLayer/Migrations/20220607115631_mig2.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogCategory");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_CategoryID",
+                table: "Blogs",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.CreateTable(
+                name: "BlogCategory",
+                columns: table => new
+                {
+                    BlogsBlogID = table.Column<int>(type: "int", nullable: false),
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogCategory", x => new { x.BlogsBlogID, x.CategoryID });
+                    table.ForeignKey(
+                        name: "FK_BlogCategory_Blogs_BlogsBlogID",
+                        column: x => x.BlogsBlogID,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BlogCategory_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogCategory_CategoryID",
+                table: "BlogCategory",
+                column: "CategoryID");
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericDal<TEntity> where TEntity : class
+    {
+        public void Delete(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public List<TEntity> GetAll()
+        {
+            using (var context = new Context())
+            {
+                return context.Set<TEntity>().ToList();
+            }
+        }
+
+        public TEntity GetByID(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Set<TEntity>().Find(id);
+            }
+        }
+
+        public void Insert(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new Context())
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
